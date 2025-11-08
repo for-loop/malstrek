7a087bd2bd6099a8849b8675f6ea4824183a9932
Hiro Watari <watari.codes@gmail.com>
2025-11-08 12:58:02 -0800

    Add timezones table

--- migrations/0004_races.up.sql
+++ migrations/0004_races.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS races (
     race_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     race_group_id INT UNSIGNED NOT NULL,
     race_number INT UNSIGNED NOT NULL UNIQUE,
+    timezone_id SMALLINT UNSIGNED NOT NULL,
     status ENUM('ON', 'TENTATIVE', 'CANCELED') default 'ON' NOT NULL,
     start_at DATETIME NOT NULL,
     end_at DATETIME NOT NULL,
@@ -11,6 +12,11 @@ CREATE TABLE IF NOT EXISTS races (
     CONSTRAINT fk_races_race_groups_race_group_id
         FOREIGN KEY (race_group_id) 
         REFERENCES race_groups (race_group_id)
+        ON DELETE CASCADE 
+        ON UPDATE RESTRICT,
+    CONSTRAINT fk_races_timezones_timezone_id
+        FOREIGN KEY (timezone_id) 
+        REFERENCES timezones (timezone_id)
         ON DELETE CASCADE 
         ON UPDATE RESTRICT
 );
