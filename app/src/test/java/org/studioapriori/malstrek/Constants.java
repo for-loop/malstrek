5728f51aee34e8b462b57a9dbae25945c9a10d4d
Hiro Watari <watari.codes@gmail.com>
2025-11-29 16:11:42 -0800

    Edit scope for constants

--- app/src/test/java/org/studioapriori/malstrek/Constants.java
+++ app/src/test/java/org/studioapriori/malstrek/Constants.java
@@ -1,8 +1,8 @@
 package org.studioapriori.malstrek;
 
 public class Constants {
-    static final String EMPTY_STRING = "";
-    static final String DOUBLE_WHITESPACE_STRING = "  ";
-    static final String NEWLINE_STRING = "\n";
-    static final String TAB_STRING = "\t";
+    public static final String EMPTY_STRING = "";
+    public static final String DOUBLE_WHITESPACE_STRING = "  ";
+    public static final String NEWLINE_STRING = "\n";
+    public static final String TAB_STRING = "\t";
 }
